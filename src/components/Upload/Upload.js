2caa5fc2f6157c486b9ed2e0cebe2878a2194b1f
Cael <2750417853@qq.com>
2022-10-26 14:36:13 +0800

    fix upload

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,15 +1,17 @@
 import React, { useRef, useState } from "react";
 import { AiFillCloseCircle } from "react-icons/ai";
 
+import BACKEND_URL from "./config";
 import { useStateContext } from "../../context/ContextProvider";
 
 const Upload = () => {
   const { setIsClickUpload } = useStateContext();
 
   const [userID, setUseID] = useState("");
+  const [userInfo, setUserInfo] = useState("");
   const [sourceFiles, setSourceFiles] = useState([]);
   const [previewImage, setPreviewImage] = useState([]);
-  const [isUploaded, setIsUploaded] = useState(true);
+  const [isUploaded, setIsUploaded] = useState(false);
   const [isUploadFailed, setIsUploadFailed] = useState(false);
 
   const hiddenFileInput = useRef();
@@ -20,9 +22,10 @@ const Upload = () => {
     if (!(userID.length && sourceFiles.length && previewImage.length)) return;
     const formData = new FormData();
     formData.append("id", userID);
+    formData.append("info", userInfo);
     previewImage.forEach((item) => formData.append("preview", item));
     sourceFiles.forEach((item) => formData.append("sources", item));
-    fetch("http://127.0.0.1:8090/api/v1/submit", {
+    fetch(BACKEND_URL + "/api/v1/submit", {
       method: "POST",
       body: formData,
     })
@@ -60,19 +63,32 @@ const Upload = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className='absolute top-0 left-0 md:w-[380px] w-[300px] h-[430px] bg-slate-700 rounded-xl p-5 flex flex-col items-center justify-between'
+      className='absolute md:-top-10 md:-left-10 top-0 left-0 md:w-[460px] w-[300px] md:h-[510px] h-[470px] bg-slate-700 rounded-xl p-5 flex flex-col gap-2 items-center justify-between'
     >
       {isUploaded && (
-        <div className='absolute top-0 left-0 md:w-[380px] w-[300px] h-[430px] bg-white bg-opacity-60 rounded-xl flex items-center justify-center duration-300'>
+        <div className='absolute top-0 left-0 md:w-[460px] w-[300px] md:h-[510px] h-[470px] bg-white bg-opacity-60 rounded-xl flex items-center justify-center duration-300'>
           <div className='flex flex-col items-center justify-center gap-5 w-2/3 h-2/3 bg-slate-700 rounded-xl'>
-            <div className='text-2xl font-semibold'>{isUploadFailed ? "上传成功" : "上传失败"}</div>
-            <button
-              type='button'
-              className='p-2 rounded-md bg-main-theme w-[100px] font-semibold'
-              onClick={() => setIsClickUpload(false)}
-            >
-              确认
-            </button>
+            <div className='text-2xl font-semibold'>{isUploadFailed ? "上传失败" : "上传成功"}</div>
+
+            <div className='flex flex-row items-center justify-center gap-3 font-semibold w-[150px] text-center'>
+              {!isUploadFailed && (
+                <a
+                  target='_blank'
+                  rel='noreferrer'
+                  href={BACKEND_URL + "/preview/" + userID}
+                  className='p-2 rounded-md bg-main-theme w-full'
+                >
+                  跳转
+                </a>
+              )}
+              <button
+                type='button'
+                className='p-2 rounded-md bg-main-theme w-full'
+                onClick={() => setIsClickUpload(false)}
+              >
+                确认
+              </button>
+            </div>
           </div>
         </div>
       )}
@@ -82,10 +98,24 @@ const Upload = () => {
         </label>
         <input
           required
-          type='text'
+          type='number'
           value={userID}
           maxLength={20}
           onChange={(e) => setUseID(e.target.value)}
+          className='bg-gray-600 rounded-md p-2 outline-none'
+        />
+      </div>
+
+      <div className='flex flex-col gap-1 w-full'>
+        <label htmlFor='id' className='text-white'>
+          简单介绍
+        </label>
+        <textarea
+          required
+          type='text'
+          value={userInfo}
+          maxLength={100}
+          onChange={(e) => setUserInfo(e.target.value)}
           className='bg-gray-600 rounded-md p-2 outline-none'
         />
       </div>
@@ -133,13 +163,13 @@ const Upload = () => {
         >
           上传网页文件
         </button>
-        <div className='flex bg-gray-600 rounded-md flex-wrap h-[120px] overflow-y-auto'>
+        <div className='flex bg-gray-600 rounded-md flex-wrap h-[80px] overflow-y-auto'>
           {sourceFiles.map((file) => (
             <File key={file.lastModified} file={file} />
           ))}
         </div>
       </div>
-      <div className='flex flex-row items-center justify-center mt-4 gap-3 font-semibold w-full'>
+      <div className='flex flex-row items-center justify-center gap-3 font-semibold w-full'>
         <button type='button' className='p-2 rounded-md bg-main-theme w-full' onClick={() => setIsClickUpload(false)}>
           取消
         </button>
